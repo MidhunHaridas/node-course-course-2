b75953ca0c0eeb0e14fae18aa30c4ea69c19d00b
Hacker <MidhunHaridas>
2017-10-16 19:10:53 +0530

    intial commot

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+
+// middleware
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method}: ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log+ '\n');
+	next();
+});
+
+/*app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname+'/public'));
+
+// partials
+hbs.registerPartials(__dirname + '/views/partials');
+// helpers
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+	// res.send('<h1>Hello express</h1>');
+	// res.send({
+	// 	name: 'Midhun',
+	// 	likes: [
+	// 			'Badminton',
+	// 			'Pc Games'
+	// 		]
+	// });
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		// currentYear: new Date().getFullYear(),
+		welcomeMsg: 'Welcome fuckers.. Fuckk OFF!!! Bitch :)'
+	});
+});
+
+app.get('/about', (req, res) => {
+	// res.send('About Page');
+	res.render('about.hbs',{
+		pageTitle: 'About Page',
+		// currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error: 'bad request'
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server started listening to http://localhost:${port}`);
+});
